0b9c9ad03e29a4f4b1ec105199d0a02a3e84f190
NSTK <mongolzkarigun@gmail.com>
2022-12-08 22:35:24 +0800

    value g damjdag bolgoh

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
-var uiController = (function() {})();
+var uiController = (function() {
+    var DomString =  {
+     inputType  :   ".add__type" , 
+     inputDescription  :  " .add__description", 
+     inputValue  :   ".add__value" ,
+     addBtn  : ".add__btn"  
+    }
+    return { 
+        getInput: function() {
+            return{ 
+                type: document.querySelector(DomString.inputType).value , 
+                description: document.querySelector(DomString.inputDescription).value ,
+                value : document.querySelector(DomString.inputValue).value 
 
-var financeController = (function() {})();
+            }
+        } ,
+        getDomStrings : function()  {
+             return DomString ; 
+        } 
+
+    }; 
+})();
+
+var financeController = (function() {
+    
+})();
 
 var appController = (function(uiController, financeController) {
+    var DOM  =  uiController.getDomStrings(); 
+
   var ctrlAddItem = function() {
-    console.log("Гараас өгөгдөл авах ");
+    // оруулах өгөлөлийг дэлгэцээс авах 
+    console.log(uiController.getInput());
+    // олж авсан өгөгдөлөө  контроллэроор дамжуулж тэнд хадлгана  
+
+    // олж авсан өгөгдөлүүдээ вэб дээр тохирох хэсэгт гаргана 
+
+    // төсөв тооцно 
+
+    //эцсийн байдлаар гаргана 
+
   };
 
-  document.querySelector(".add__btn").addEventListener("click", function() {
+  document.querySelector(DOM.addBtn).addEventListener("click", function() {
     ctrlAddItem();
   });
 
